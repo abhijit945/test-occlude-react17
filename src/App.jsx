dbc217b808d16a616d6f444b9a152c510d513613
Abhijit Rao <abhijit945@gmail.com>
2023-05-16 16:35:27 +0000

    Update 2 files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import { useState, useRef } from 'react'
+import { Suspense, useState, useRef } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { useGLTF, Html, OrbitControls, Environment, ContactShadows } from '@react-three/drei'
 import { FaMapMarkerAlt } from 'react-icons/fa'
@@ -66,12 +66,14 @@ function Marker({ children, ...props }) {
 
 export default function Viewer() {
   return (
-    <Canvas camera={{ position: [5, 0, 0], fov: 50 }}>
-      <ambientLight intensity={0.5} />
-      <Model position={[0, 0.25, 0]} />
-      <Environment preset="city" />
-      <ContactShadows frames={1} scale={5} position={[0, -1, 0]} far={1} blur={5} opacity={0.5} color="#204080" />
-      <OrbitControls />
+    <Canvas gl={{ preserveDrawingBuffer: true }} shadows dpr={[1, 2]} camera={{ position: [5, 0, 0], fov: 50 }}>
+      <Suspense fallback={<>Loading...</>}>
+        <ambientLight intensity={0.5} />
+        <Model position={[0, 0.25, 0]} />
+        <Environment preset="city" />
+        <ContactShadows frames={1} scale={5} position={[0, -1, 0]} far={1} blur={5} opacity={0.5} color="#204080" />
+        <OrbitControls />
+      </Suspense>
     </Canvas>
   )
 }
